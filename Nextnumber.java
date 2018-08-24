4fb0d531cac5a190b7874a4b492c434b61dce0e7
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-24 00:55:00 -0400

    Create Nextnumber.java

---
+++ Nextnumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Nextnumber
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		n++;
+		System.out.println(n);
+	}
+}
