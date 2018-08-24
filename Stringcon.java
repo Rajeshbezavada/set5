7a01ed268332d458bf36d4bf675cf0c017edeca1
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-24 00:24:59 -0400

    Create Stringcon.java

---
+++ Stringcon.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Stringcon
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str1=sc.next();
+		String str2=sc.next();
+		System.out.println(str1+str2);
+	}
+}
