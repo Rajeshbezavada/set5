7ba63e120c2b343f530c88b23163b9f661e907cf
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-24 00:01:09 -0400

    Create Stringcount.java

---
+++ Stringcount.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Stringcount
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(str);
+		}
+	}
+}
